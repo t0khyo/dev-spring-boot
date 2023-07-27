e540afcf63be8f6d15abeeefd36a820ae288251d
t0khyo <abdullrhman.eltoukhy@gmail.com>
2023-07-27 15:33:11 +0300

    02-dev-tools-demo created

---
+++ 01-spring-boot-overview/02-dev-tools-demo/src/main/java/com/t0khyo/mycoolapp/MycoolappApplication.java
@@ -0,0 +1,13 @@
+package com.t0khyo.mycoolapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MycoolappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MycoolappApplication.class, args);
+	}
+
+}
