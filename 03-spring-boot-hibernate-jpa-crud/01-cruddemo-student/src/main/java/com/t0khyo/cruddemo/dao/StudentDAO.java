7c77bee3b800cd9789a4b65fcb3200c7df85faeb
t0khyo <abdullrhman.eltoukhy@gmail.com>
2023-07-28 19:40:57 +0300

    Querying Objects with JPA

--- 03-spring-boot-hibernate-jpa-crud/01-cruddemo-student/src/main/java/com/t0khyo/cruddemo/dao/StudentDAO.java
+++ 03-spring-boot-hibernate-jpa-crud/01-cruddemo-student/src/main/java/com/t0khyo/cruddemo/dao/StudentDAO.java
@@ -2,8 +2,12 @@ package com.t0khyo.cruddemo.dao;
 
 import com.t0khyo.cruddemo.entity.Student;
 
+import java.util.List;
+
 public interface StudentDAO {
     void save(Student theStudent);
 
     Student findById(Integer id);
+
+    List<Student> findAll();
 }
--- 03-spring-boot-hibernate-jpa-crud/01-cruddemo-student/src/main/java/com/t0khyo/cruddemo/dao/StudentDAOImpl.java
+++ 03-spring-boot-hibernate-jpa-crud/01-cruddemo-student/src/main/java/com/t0khyo/cruddemo/dao/StudentDAOImpl.java
@@ -2,9 +2,12 @@ package com.t0khyo.cruddemo.dao;
 
 import com.t0khyo.cruddemo.entity.Student;
 import jakarta.persistence.EntityManager;
+import jakarta.persistence.TypedQuery;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Repository
 public class StudentDAOImpl implements StudentDAO {
@@ -28,5 +31,14 @@ public class StudentDAOImpl implements StudentDAO {
     @Override
     public Student findById(Integer id) {
         return entityManager.find(Student.class, id);
+    }
+
+    @Override
+    public List<Student> findAll() {
+        // create query - based on the JPA Entity
+        TypedQuery<Student> theQuery =entityManager.createQuery("FROM Student", Student.class);
+
+        // return query results
+        return theQuery.getResultList();
     }
 }
