3895d7a99f895c1e958d3164e88e973e0665b62c
t0khyo <abdullrhman.eltoukhy@gmail.com>
2023-07-28 17:56:26 +0300

    run spring boot app from the command line

---
+++ 01-spring-boot-overview/05-command-line-demo/src/main/java/com/t0khyo/mycoolapp/Controller/FunRestController.java
@@ -0,0 +1,22 @@
+package com.t0khyo.mycoolapp.Controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class FunRestController {
+
+    // expose "/" that return "Hello World!"
+    @GetMapping("/")
+    public String sayHello() {
+        return "Hello world!";
+    }
+
+    // expose a new endpoint for workout
+    @GetMapping("/workout")
+    public String getDailyWorkout() {
+        return "Run a hard 5k!";
+    }
+
+    //
+}
