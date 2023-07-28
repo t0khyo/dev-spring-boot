e06febc8f77325cd82e1bfdd28b359cb7efd00c0
t0khyo <abdullrhman.eltoukhy@gmail.com>
2023-07-28 19:46:28 +0300

    sort results ascending by last name

--- 03-spring-boot-hibernate-jpa-crud/01-cruddemo-student/src/main/java/com/t0khyo/cruddemo/dao/StudentDAOImpl.java
+++ 03-spring-boot-hibernate-jpa-crud/01-cruddemo-student/src/main/java/com/t0khyo/cruddemo/dao/StudentDAOImpl.java
@@ -36,7 +36,7 @@ public class StudentDAOImpl implements StudentDAO {
     @Override
     public List<Student> findAll() {
         // create query - based on the JPA Entity
-        TypedQuery<Student> theQuery =entityManager.createQuery("FROM Student", Student.class);
+        TypedQuery<Student> theQuery =entityManager.createQuery("FROM Student ORDER BY lastName ASC", Student.class);
 
         // return query results
         return theQuery.getResultList();
