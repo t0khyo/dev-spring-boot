890042ea6f3de945f35c9fa1eafd24ad58d154e0
t0khyo <abdullrhman.eltoukhy@gmail.com>
2023-08-03 12:45:43 +0300

    Refactor getStudents(), make use of @PostConstruct - more efficent in our use of loading data in creating the students

--- 04-spring-boot-rest-crud/01-spring-boot-rest-crud/src/main/java/com/t0khyo/demo/rest/StudentRestController.java
+++ 04-spring-boot-rest-crud/01-spring-boot-rest-crud/src/main/java/com/t0khyo/demo/rest/StudentRestController.java
@@ -1,6 +1,7 @@
 package com.t0khyo.demo.rest;
 
 import com.t0khyo.demo.entity.Student;
+import jakarta.annotation.PostConstruct;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,15 +13,23 @@ import java.util.List;
 @RequestMapping("/api")
 public class StudentRestController {
 
-    // define endpoint for "/students" - return a list of students
-    @GetMapping("/students")
-    public List<Student> getStudents() {
-        List<Student> theStudents = new ArrayList<>();
+    List<Student> theStudents;
+
+    // define @PostConstruct to load the students data ... only once!
+    @PostConstruct
+    public void loadData() {
+        theStudents = new ArrayList<>();
 
         theStudents.add(new Student("Mohamed", "Abdeljalil"));
         theStudents.add(new Student("Abdelrahman", "Eltokhy"));
         theStudents.add(new Student("Mazen", "Sedik"));
+    }
 
+    // define endpoint for "/students" - return a list of students
+    @GetMapping("/students")
+    public List<Student> getStudents() {
         return theStudents;
     }
+
 }
+
