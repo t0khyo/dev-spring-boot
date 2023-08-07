936fc16d9f3b812aba5cb05baa34e1b84950c924
t0khyo <abdullrhman.eltoukhy@gmail.com>
2023-08-07 13:08:53 +0300

    add mapping for DELETE /employees/{employeeId} - delete employee

--- 04-spring-boot-rest-crud/02-spring-boot-rest-crud-employee/src/main/java/com/t0khyo/springboot/cruddemo/controller/EmployeeRestController.java
+++ 04-spring-boot-rest-crud/02-spring-boot-rest-crud-employee/src/main/java/com/t0khyo/springboot/cruddemo/controller/EmployeeRestController.java
@@ -56,4 +56,18 @@ public class EmployeeRestController {
         return dbEmployee;
     }
 
+    // add mapping for DELETE /employees/{employeeId} - delete employee
+    @DeleteMapping("/employees/{employeeId}")
+    public String deleteEmployee(@PathVariable int employeeId) {
+        Employee tempEmployee = employeeService.findById(employeeId);
+
+        if (tempEmployee == null) {
+            throw new RuntimeException("Employee id not found - " + employeeId);
+        }
+
+        employeeService.deleteById(employeeId);
+
+        return "Deleted employee id - " + employeeId;
+    }
+
 }
