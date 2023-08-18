e76606f93d9155884aa2e18b56f098df326f8769
t0khyo <abdullrhman.eltoukhy@gmail.com>
2023-08-18 22:16:22 +0300

    set up 01-mvc-security-defualt

---
+++ 08-spring-boot-mvc-security/01-spring-boot-mvc-security-default/src/main/java/com/t0khyo/springboot/demosecurity/controller/DemoController.java
@@ -0,0 +1,12 @@
+package com.t0khyo.springboot.demosecurity.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class DemoController {
+    @GetMapping("/")
+    public String showHome() {
+        return "home";
+    }
+}
