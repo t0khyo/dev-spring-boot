ea2b89cbb4fb6cefcdc87e3e0ba40f8795c85783
t0khyo <abdullrhman.eltoukhy@gmail.com>
2023-08-19 16:00:13 +0300

    MVC securit - custom login form

---
+++ 08-spring-boot-mvc-security/01-spring-boot-mvc-security-default/src/main/java/com/t0khyo/springboot/demosecurity/security/DemoSecurityConfig.java
@@ -0,0 +1,53 @@
+package com.t0khyo.springboot.demosecurity.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class DemoSecurityConfig {
+    @Bean
+    public InMemoryUserDetailsManager userDetailsManager() {
+        UserDetails abdelrahman = User.builder()
+                .username("abdelrahman")
+                .password("{noop}test123")
+                .roles("EMPLOYEE", "MANAGER", "ADMIN")
+                .build();
+
+        UserDetails mohamed = User.builder()
+                .username("mohamed")
+                .password("{noop}test123")
+                .roles("EMPLOYEE", "MANAGER")
+                .build();
+
+        UserDetails ahmed = User.builder()
+                .username("ahmed")
+                .password("{noop}test123")
+                .roles("EMPLOYEE", "MANAGER")
+                .build();
+
+        return new InMemoryUserDetailsManager(abdelrahman, mohamed, ahmed);
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests(
+                        configurer ->
+                                configurer
+                                        .anyRequest()
+                                        .authenticated()
+                )
+                .formLogin(formLoginConfigurer ->
+                        formLoginConfigurer
+                                .loginPage("/showMyLoginPage")
+                                .loginProcessingUrl("/authenticateTheUser")
+                                .permitAll()
+                );
+
+        return http.build();
+    }
+}
