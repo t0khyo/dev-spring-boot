005d9bbce824262cae5ca89aa86c26552ec1638c
t0khyo <abdullrhman.eltoukhy@gmail.com>
2023-08-23 13:05:23 +0300

    set up 04- OneToMany Unidirectionl

---
+++ 09-spring-boot-jpa-advanced-mappings/04-jpa-one-to-many-uni/src/main/java/com/t0khyo/cruddemo/dao/AppDAO.java
@@ -0,0 +1,33 @@
+package com.t0khyo.cruddemo.dao;
+
+import com.t0khyo.cruddemo.entity.Course;
+import com.t0khyo.cruddemo.entity.Instructor;
+import com.t0khyo.cruddemo.entity.InstructorDetail;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface AppDAO {
+    void save(Instructor theInstructor);
+
+    Instructor findInstructorById(int theId);
+
+    void deleteInstructorById(int theId);
+
+    InstructorDetail findInstructorDetailById(int theId);
+
+    void deleteConstructorDetailById(int theId);
+
+    List<Course> findCoursesByInstructorId(int theId);
+
+    Instructor findInstructorByIdJoinFetch(int theId);
+
+    void update(Instructor theInstructor);
+
+    void update(Course theCourse);
+
+    Course findCourseById(int theId);
+
+    void deleteCourseById(int theId);
+}
